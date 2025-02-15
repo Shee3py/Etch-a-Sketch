d1f27f700ae0791e7155ee173983bcbbac2b831b
Shee3py <96235652+Shee3py@users.noreply.github.com>
2025-02-16 00:44:25 +0400

    created grid with size variation depending on box count

--- etch.js
+++ etch.js
@@ -2,7 +2,17 @@ let container = document.querySelector("#container");
 let gridSize = 16;
 let height = [];
 let length = [];
-let blockSize = num.toString(960 / gridSize) + "px";
+let blockSize = 960 / gridSize;
+blockSize = blockSize.toString() + "px";
+
+function colorChange(div){
+    if(div.style.backgroundColor == "black"){
+        div.style.backgroundColor == "white";
+    }else {
+        div.style.backgroundColor == "black";
+    }
+
+}
 
 for(let i = 0; i < gridSize; i++){
     height.push(document.createElement("div"));
@@ -14,6 +24,7 @@ for(let i = 0; i < gridSize; i++){
         div.classList.add("block");
         div.style.width = blockSize;
         div.style.height = blockSize;
+        div.addEventListener("mouseover", () => colorChange(div))
         height[i].appendChild(div);
     }
 }
